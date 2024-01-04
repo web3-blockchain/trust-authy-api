31bd725ae54848ae9d37f047c4b61369b06623c2
iida <web3.block.tech@gmail.com>
2024-01-05 08:50:02 +0900

    feat

--- src/pages/api/mintTo.ts
+++ src/pages/api/mintTo.ts
@@ -1,10 +1,18 @@
 import { mintTo } from "@/lib/ERC721TA/mintTo";
 import { env } from "@/lib/config";
 
+function isValidEthereumAddress(address: string) {
+  return /^0x[a-fA-F0-9]{40}$/.test(address);
+}
+
 export default async function handler(req: any, res: any) {
   if (req.method === 'POST') {
     console.log(req.body);
     const toAddress = req.body.to
+    if (!toAddress || !isValidEthereumAddress(toAddress)) {
+      return res.status(400).json({ error: 'Invalid Ethereum address' });
+    }
+
     const tx = await mintTo(toAddress, env.SBT_METADATA_URI);
 
     res.status(200).json({ transaction: tx });
